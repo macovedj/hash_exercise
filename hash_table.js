7879240e7d8b0603c0c29cf10634ece45fb040a5
Daniel Macovei <djmacovei@gmail.com>
2018-06-30 12:35:03 -0400

    initial commit

---
+++ hash_table.js
@@ -0,0 +1,16 @@
+const hashTable = () => {
+    let keys = [];
+    let indices = [];
+
+    const put = (key, index) => {
+        return 'you tried to push!!!';
+    }
+
+    const get = (key) => {
+        return 'you tried to get!!!';
+    }
+
+    return { put, get }
+}
+
+export default hashTable;
